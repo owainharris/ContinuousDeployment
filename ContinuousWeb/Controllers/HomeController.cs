04a82be0fefdbf3f416175db7c2c7cf1beb57390
Owain Harris <owainh@gmail.com>
2017-10-27 00:59:21 -0400

    added schedular

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -16,11 +16,13 @@ namespace ContinuousWeb.Controllers
         }
 
         public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+{
+    throw new NotImplementedException();
 
-            return View();
-        }
+    //ViewData["Message"] = "Your application description page.";
+    //return View();
+}
+
 
         public IActionResult Contact()
         {
